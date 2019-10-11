27d7e8bcfda0579efc7ba70dc1d7235178ae8204
Dane Stuckel <dane.stuckel@gmail.com>
2019-10-11 14:48:09 -0400

    finalized version of typescript types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,24 +1,39 @@
 // Type definitions for client-side-event-bus
 
 // it's a global when in non-module environments
+import {ClientSideEventBus} from "./index";
+
 export as namespace Bus;
 
 // it's a class that can be imported
 export = Bus;
 
-declare class Ring {
-  constructor(max: number);
-
-  push(item: any): void;
-  asArray(): any[];
-  list: any[];
-}
-
-declare class Bus {
+declare class Bus implements ClientSideEventBus {
   constructor();
 
   on(topicStr: string, fn: () => void): () => void;
   emit(topicStr: string, message?: any): Promise<any>[];
   history(topicStr: string): Record<string, number>[]
-  static Ring: typeof Ring;
+}
+
+declare namespace Bus {
+  interface ClientSideEventBus {
+    on(topicStr: string, fn: () => void): () => void;
+    emit(topicStr: string, message?: any): Promise<any>[];
+    history(topicStr: string): Record<string, number>[]
+  }
+
+  interface RingBuffer {
+    push(item: any): void;
+    asArray(): any[];
+    list: any[];
+  }
+
+  export class Ring implements RingBuffer {
+    constructor(max: number);
+
+    push(item: any): void;
+    asArray(): any[];
+    list: any[];
+  }
 }
